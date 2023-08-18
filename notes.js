c8eb4a7c1ca678816fed1f4f0858c56cb9eb4d64
kenkapo <58553879+kenkapo@users.noreply.github.com>
2023-08-18 11:18:34 +0530

    Update notes.js

--- notes.js
+++ notes.js
@@ -1,5 +1,6 @@
-1. Promise:
+1. Promise: The Promise object represents the eventual completion (or failure) of an asynchronous operation and its resulting value.
   it has three keys:
   a. prototype
   b. promise state : tells the state of promise
   c. promise result : stores the result given by API.
+  
