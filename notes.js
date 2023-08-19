134d6d9d536fe95b310e0c41924fe9d93706fc96
kenkapo <58553879+kenkapo@users.noreply.github.com>
2023-08-19 12:15:35 +0530

    Update notes.js

--- notes.js
+++ notes.js
@@ -5,7 +5,30 @@
   c. promise result : stores the result given by API.
   
 2. Tailwind css components has codes both for phones and bigger screens like laptops.
+  
 3. we can pass other components as children to react components.
 
 4. productlist has ->product list as well as category filters.
+  
 5. pages folder is used to store complete pages.
+  
+6.React Router is a popular library for handling routing in React applications. 
+  Routing is the process of managing the URLs of a web application and rendering different components based on the URL. 
+  React Router helps you create single-page applications (SPAs) where the content changes dynamically without requiring a full page reload.
+
+7.It's important to note that the <Link> component uses client-side routing to update the URL without causing a full page reload. 
+  Anchor tags cause full page reload.
+
+8.The <select> element is used to create a drop-down list.
+
+9.The htmlFor property sets or returns the value of the for attribute of a label.
+
+10.In the checkout page we have divided the screen in 5 columns and given form 3 cols and cart 2 cols.
+
+11.When using Icons in tailwind css , put them as a component like we did in Material UI.
+
+12. we used json server npm package that helps us to create fake REST API.
+
+13.You can think of an API as a mediator between the users or clients and the resources or web services they want to get. It’s also a way for an organization to share resources and information while maintaining security, control, and authentication—determining who gets access to what. 
+
+14. Post->Create , Get->Read , Put and Patch->Update , Delete->Delete.
